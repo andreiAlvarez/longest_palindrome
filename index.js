9e13adc39877a9d5d27dfb2c280d6345e61e1ae0
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2020-09-08 09:29:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
+// solution 1
 longestPalindrome=function(s){
   var longest = 0;
-  var length = s.length;
   
-  for (var i = 0; i < length; i++) {
-    for (var j = i + 1; j <= length; j++) {
+  for (var i = 0; i < s.length; i++) {
+    for (var j = i + 1; j <= s.length; j++) {
       var str = s.slice(i, j);
-      var l = str.length;
-      if (isPalindrome(str) && longest < l) {
-        longest = l;
+      if (isPalindrome(str) && longest < str.length) {
+        longest = str.length;
       }
     }
   }
@@ -17,4 +16,16 @@ longestPalindrome=function(s){
 function isPalindrome(s) {
   var arr = s.split("");
   return s == arr.reverse().join("");
+}
+
+//clever
+
+var longestPalindrome=function(s){
+  if (!s) return 0;
+  for (let c = s.length; c > 0; c--) {
+    for (let i = 0; i <= s.length - c; i++) {
+      var check = s.substr(i, c);
+      if (check === check.split("").reverse().join("")) return c;
+    }
+  }
 }
